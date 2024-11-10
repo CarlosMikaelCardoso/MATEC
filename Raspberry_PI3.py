3a1c67f520bc4fdbed424b011cd46d0010d5585a
Carlos Mikael Cardoso Da Costa <163159218+CarlosMikaelCardoso@users.noreply.github.com>
2024-11-10 19:58:53 -0300

    Update Raspberry_PI3.py

--- Raspberry_PI3.py
+++ Raspberry_PI3.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 # Configuração para notificar via Pushbullet
 PUSHBULLET_API_KEY = "o.GDsMyFtRG4IVAfvpgVJKRnyHKiVYjZuV"  # Insira sua chave de API aqui
-temperature_alert_sent = False  # Variável de controle para o envio da notificação
+temperature_alert_sent = False  # Variável de controle para o envio da notificação de temperatura
 power_alert_sent = False  # Variável de controle para o envio do alerta de queda de energia
+sound_alert_sent = False  # Variável de controle para o envio do alerta de som
 
 def send_push_notification(title, body):
     url = "https://api.pushbullet.com/v2/pushes"
@@ -27,31 +28,41 @@ def send_push_notification(title, body):
 
 @app.route('/endpoint', methods=['POST'])
 def receive_data():
-    global temperature_alert_sent
-    global power_alert_sent
+    global temperature_alert_sent, power_alert_sent, sound_alert_sent
     
     data = request.get_json()
     
-    if data and "temperature" in data:
-        temperature = data["temperature"]
-        print(f"Temperatura recebida: {temperature}°C")
-        
-        if temperature >= 30 and not temperature_alert_sent:
-            print("Alerta: Temperatura acima de 30°C!")
-            send_push_notification("Alerta de Temperatura", f"Temperatura chegou a {temperature}°C!")
-            temperature_alert_sent = True  # Marca que o alerta foi enviado
-        elif temperature < 30 and temperature_alert_sent:
-            print("Alerta: Temperatura abaixo de 30°C!")
-            send_push_notification("Alerta de Temperatura", f"Temperatura diminuída para {temperature}°C!")
-            temperature_alert_sent = False  # Reseta o estado quando a temperatura cai abaixo de 30°C
+    if data:
+        # Verifica alerta de temperatura
+        if "temperature" in data:
+            temperature = data["temperature"]
+            print(f"Temperatura recebida: {temperature}°C")
             
-        return jsonify({"status": "success"}), 200
-    
-    elif data and "power" in data and data["power"] == "off":
-        print("Alerta: Queda de energia detectada!")
-        if not power_alert_sent:
-            send_push_notification("Alerta de Queda de Energia", "A energia caiu!")
-            power_alert_sent = True  # Marca que o alerta foi enviado
+            if temperature >= 30 and not temperature_alert_sent:
+                print("Alerta: Temperatura acima de 30°C!")
+                send_push_notification("Alerta de Temperatura", f"Temperatura chegou a {temperature}°C!")
+                temperature_alert_sent = True  # Marca que o alerta foi enviado
+            elif temperature < 30 and temperature_alert_sent:
+                print("Alerta: Temperatura abaixo de 30°C!")
+                send_push_notification("Alerta de Temperatura", f"Temperatura diminuída para {temperature}°C!")
+                temperature_alert_sent = False  # Reseta o estado quando a temperatura cai abaixo de 30°C
+        
+        # Verifica alerta de queda de energia
+        if "power" in data and data["power"] == "off":
+            print("Alerta: Queda de energia detectada!")
+            if not power_alert_sent:
+                send_push_notification("Alerta de Queda de Energia", "A energia caiu!")
+                power_alert_sent = True  # Marca que o alerta foi enviado
+                
+        # Verifica alerta de som
+        if "sound" in data and data["sound"] == "detected":
+            print("Alerta: Som detectado!")
+            if not sound_alert_sent:
+                send_push_notification("Alerta de Som", "Um som foi detectado!")
+                sound_alert_sent = True  # Marca que o alerta de som foi enviado
+        elif "sound" in data and data["sound"] != "detected":
+            sound_alert_sent = False  # Reseta o alerta de som quando não há som detectado
+            
         return jsonify({"status": "success"}), 200
     
     else:
