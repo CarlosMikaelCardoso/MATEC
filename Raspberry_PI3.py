d08ed7c243218363326a46b3baa9b72679185c62
Carlos Mikael Cardoso Da Costa <163159218+CarlosMikaelCardoso@users.noreply.github.com>
2025-01-14 16:32:07 -0300

    Update Raspberry_PI3.py

--- Raspberry_PI3.py
+++ Raspberry_PI3.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 # Configuração para notificar via Pushbullet
 PUSHBULLET_API_KEY = "o.GDsMyFtRG4IVAfvpgVJKRnyHKiVYjZuV"  # Chave API
+PUSHBULLET_API_KEY2 = "o.lbovkynEFTscuGNVxbmEFxSrwqfGYtTK"
 temperature_alert_sent = False  # Variável de controle para o envio da notificação de temperatura
 power_alert_sent = False  # Variável de controle para o envio do alerta de queda de energia
 sound_alert_sent = False  # Variável de controle para o envio do alerta de som
@@ -13,6 +14,23 @@ def send_push_notification(title, body):
     url = "https://api.pushbullet.com/v2/pushes"
     headers = {
         "Access-Token": PUSHBULLET_API_KEY,
+        "Content-Type": "application/json"
+    }
+    data = {
+        "type": "note",
+        "title": title,
+        "body": body
+    }
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        print("Notificação enviada com sucesso!")
+    else:
+        print("Falha ao enviar notificação:", response.status_code, response.text)
+
+def send_push_notification2(title, body):
+    url = "https://api.pushbullet.com/v2/pushes"
+    headers = {
+        "Access-Token": PUSHBULLET_API_KEY2,
         "Content-Type": "application/json"
     }
     data = {
@@ -41,10 +59,12 @@ def receive_data():
             if temperature >= 30 and not temperature_alert_sent:
                 print("Alerta: Temperatura acima de 30°C!")
                 send_push_notification("Alerta de Temperatura", f"Temperatura chegou a {temperature}°C!, Acionando Ventilador")
+                send_push_notification2("Alerta de Temperatura", f"Temperatura chegou a {temperature}°C!, Acionando Ventilador")
                 temperature_alert_sent = True  # Marca que o alerta foi enviado
             elif temperature < 30 and temperature_alert_sent:
                 print("Alerta: Temperatura abaixo de 30°C!")
                 send_push_notification("Alerta de Temperatura", f"Temperatura diminuída para {temperature}°C!, Ventilador Desligado")
+                send_push_notification2("Alerta de Temperatura", f"Temperatura diminuída para {temperature}°C!, Ventilador Desligado")
                 temperature_alert_sent = False  # Reseta o estado quando a temperatura cai abaixo de 30°C
         
         # Verifica alerta de queda de energia
@@ -52,6 +72,7 @@ def receive_data():
             print("Alerta: Queda de energia detectada!")
             if not power_alert_sent:
                 send_push_notification("Alerta de Queda de Energia", "A energia caiu!")
+                send_push_notification2("Alerta de Queda de Energia", "A energia caiu!")
                 power_alert_sent = True  # Marca que o alerta foi enviado
                 
         # Verifica alerta de som
@@ -59,6 +80,7 @@ def receive_data():
             print("Alerta: Som detectado!")
             if not sound_alert_sent:
                 send_push_notification("Alerta de Som", "Um som foi detectado!")
+                send_push_notification2("Alerta de Som", "Um som foi detectado!")
                 sound_alert_sent = True  # Marca que o alerta de som foi enviado
         else:
             sound_alert_sent = False  # Reseta o alerta de som quando não há som detectado
